3e9d4d7eaea7c2320732ee6ff830a13d35d51a99
sherin <sherinsaju123@gmail.com>
2022-05-18 05:59:12 +0530

    messeage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Viewuser from './component/Viewuser';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Viewuser/>
     </div>
   );
 }
